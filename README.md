e496d1d33bfebd79a54d443fca9956ba06657e23
Lewis Luo <ts03002215@gmail.com>
2020-10-14 03:41:10 +0800

    feat: add order filter

--- README.md
+++ README.md
@@ -7,26 +7,27 @@
 
 4. 可以新增、修改、刪除每筆餐廳資料
 
-# 使用的軟體與套件
+5. 可以利用「排序」選單餐廳將列表依照選項排序
+
+# 開發使用的軟體與套件
 1. VS code
 2. Node.js v10.15.0
 3. Nodemon v2.0.4
 4. Express v4.17.1
 5. Express-handlebars v5.1.0
-6. Bootstrap v4.5.2
-7. Jquery v3.5.1
-8. Popper v2.5.2
-9. Font Awesome Free v5.14.0
+6. Body-parser v1.19.0
+7. Method-override v3.0.0
+8. Mongoose v5.10.7
+9. Bootstrap v4.5.2
+10. Font Awesome Free v5.14.0
 
 # 安裝流程
 1. 於terminal中輸入複製專案到電腦中。<br>
 <code>$ git clone https://github.com/LewisLuo/favorite_restaurants</code>
-2. 到專案資料夾中使用npm安裝套件。<br>
+2. 進入專案資料夾中使用npm安裝套件。<br>
 <code>$ cd favorite_restaurants</code><br>
-<code>$ npm install</code>
-3. 使用node.js或nodemon開始本機伺服器。<br>
+<code>$ npm init</code>
+3. 使用node.js或nodemon開啟本機伺服器。<br>
 <code>$ node app.js</code>
-4. 若成功開啟時terminal會傳入以下訊息。<br>
-<code>Express is running on http://localhost:3000</code>
-5. 進入<code>http://localhost:3000</code>即可使用本專案。
-
+4. 成功開啟時終端機會傳入以下訊息。<br>
+<code>Express server is running on http://localhost:3000</code>
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,9 +4,38 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
+  let sortFilter, order, orderName
+
+  switch (req.query.sort) {
+    case 'name':
+      sortFilter = 'name'
+      order = 'asc'
+      orderName = '餐廳名 (A -> Z)'
+      break;
+    case 'name-r':
+      sortFilter = 'name'
+      order = 'desc'
+      orderName = '餐廳名 (Z -> A)'
+      break;
+    case 'category':
+      sortFilter = 'category'
+      order = 'asc'
+      orderName = '類別'
+      break;
+    case 'location':
+      sortFilter = 'location'
+      order = 'asc'
+      orderName = '地區'
+    default:
+      sortFilter = '_id'
+      order = 'asc'
+      orderName = '排序'
+  }
+
   Restaurant.find()
     .lean()
-    .then((restaurants) => res.render('index', { restaurants: restaurants }))
+    .sort({ [sortFilter]: [order] })
+    .then((restaurants) => res.render('index', { restaurants: restaurants, orderName }))
     .catch(error => { console.log('Error from mongoose-index') })
 })
 
